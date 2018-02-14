11f7b03011fb87a90b7c8a91ad4195f8b4ad07d9
William Durand <will+git@drnd.me>
2018-02-14 11:04:11 +0100

    Add tests for the auth reducer

--- src/shared/actions/auth.js
+++ src/shared/actions/auth.js
@@ -4,7 +4,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { AUTH_SIGNIN, AUTH_SIGNOUT, FETCH_REQUEST, FETCH_SUCCESS, FETCH_FAILURE } from "./";
+import {
+  AUTH_SIGNIN,
+  AUTH_SIGNOUT,
+  FETCH_REQUEST,
+  FETCH_SUCCESS,
+  FETCH_FAILURE,
+} from "./";
 
 export function signIn({ provider, username, password }) {
   return {
