d451e9d0bfc267e1ffffd06979420bc7c50362a7
William Durand <will+git@drnd.me>
2018-02-15 18:53:04 +0100

    Add default config

--- src/shared/config.js
+++ src/shared/config.js
@@ -4,4 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-// TODO
+
+// This file should be aliased in webpack configuration.
+export default {};
