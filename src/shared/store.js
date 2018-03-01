9a05ca328440d826fa242efce3365d52a7933bca
William Durand <will+git@drnd.me>
2018-03-01 20:46:24 +0100

    Cleanup store

--- src/shared/store.js
+++ src/shared/store.js
@@ -6,14 +6,14 @@
  */
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
+
 import reducers from "./reducers";
 import rootSaga from "./sagas";
 
 /* global window */
 export default function configureStore(extReducers, extSaga, initialState = {}) {
   const sagaMiddleware = createSagaMiddleware();
-  // TODO combine reducers / extReducers
-  // TODO combine rootSaga / extSaga
+
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
@@ -21,27 +21,6 @@ export default function configureStore(extReducers, extSaga, initialState = {}) 
     composeEnhancers(applyMiddleware(sagaMiddleware)),
   );
   sagaMiddleware.run(rootSaga);
-  /* eslint-enable no-underscore-dangle */
-
-  /* global module */
-  /* eslint-disable global-require */
-  /* eslint-disable no-undef */
-  if (module.hot) {
-    module.hot.accept("./reducers", () => {
-      // console.log("HMR reducers");
-      // TODO add extReducers too
-      store.replaceReducer(require("./reducers").default);
-    });
-    module.hot.accept("./sagas", () => {
-      // console.log("HMR sagas TODO");
-      // TODO reload Sagas
-      // @see https://gist.github.com/markerikson/dc6cee36b5b6f8d718f2e24a249e0491
-      // SagaManager.cancelSagas(store);
-      // require("./sagas").default.startSagas(sagaMiddleware);
-    });
-  }
-  /* eslint-enable no-undef */
-  /* eslint-enable global-require */
 
   return store;
 }
