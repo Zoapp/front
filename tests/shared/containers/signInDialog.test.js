eaa8d8745792823c5de43105c38fc131aae0eaae
William Durand <will+git@drnd.me>
2018-03-21 11:49:51 +0100

    Make tests pass

--- tests/shared/containers/signInDialog.test.js
+++ tests/shared/containers/signInDialog.test.js
@@ -7,20 +7,36 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
+import Zrmc from "zrmc";
 
 import { createFakeEvent } from "tests/helpers";
 import { SignInDialogBase } from "shared/containers/signInDialog";
 
 describe("containers/signInDialog", () => {
   it("renders correctly", () => {
-    const signInSpy = jest.fn();
-    const component = renderer.create(<SignInDialogBase signIn={signInSpy} />);
+    // We are interested in seeing whether this method is called.
+    const showModalSpy = jest.fn();
+
+    // This represents the fake `dialogRef`
+    const createNodeMock = () => ({
+      addEventListener: jest.fn(),
+      showModal: showModalSpy,
+    });
+
+    const component = renderer.create(<SignInDialogBase signIn={jest.fn()} />, {
+      createNodeMock,
+    });
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+
+    expect(showModalSpy).toHaveBeenCalled();
   });
 
   it("sign in with fullfilled form", () => {
+    // Override Zrmc to make sure it is called
+    Zrmc.closeDialog = jest.fn();
+
     const signInSpy = jest.fn();
     const wrapper = shallow(<SignInDialogBase signIn={signInSpy} />);
 
@@ -34,8 +50,12 @@ describe("containers/signInDialog", () => {
       .simulate("change", { target: { value: "password" } });
     expect(wrapper.state("password")).toEqual("password");
 
+    expect(signInSpy).not.toHaveBeenCalled();
+    expect(Zrmc.closeDialog).not.toHaveBeenCalled();
+
     wrapper.find("#signin-dialog-form").simulate("submit", createFakeEvent());
 
     expect(signInSpy).toHaveBeenCalled();
+    expect(Zrmc.closeDialog).toHaveBeenCalled();
   });
 });
